4f293748f6961d5548230ce3e5ba9e9b29b8d4fb
Alimzy <salamialameen20@gmail.com>
2026-05-13 00:42:05 +0100

    completed get transaction summary

--- pybank.py
+++ pybank.py
@@ -34,5 +34,40 @@ def is_strong(password):
         return True
     return False
 print(is_strong("mydfgrtyuh"))
+
+def apply_interest(balance,rate,years):
+    if rate < 0 and year < 1:
+        raise ValueError("This is invalid input")
+    interest = blance(1 +rate)**years
+    return interest
+
+print(apply_interest(5000,123,1))
+
+
+def get_transaction_summary(transactions):
+    total_credits = 0
+    total_debits = 0
+    for transaction in transactions:
+        if transaction[0] == "credit":
+            total_credits += transaction[1]
+         if transaction[0] == "debit":
+            total_debits += transaction[1]
+    net_balance = total_credits - total_debits
+    transaction_count = len(transactiona)
+    output = [["total_credits", total_credits],["total_debits", total_debits],["net_balance",net_balance], ["transaction_count",transaction_count]]
+    return output
+
+transactions = [
+["credit", 2000],
+["debit", 500],
+["credit", 300],
+]
+print(get_transaction_summary(transactions))
+
+   
+
+
+        
+
         
             
